50e2489a6631829101ceb61865d51db79a06ca19
suzu <miyuki.takahashi.0526@gmail.com>
2023-09-19 23:27:49 -0400

    fix birds height

--- src/components/animation/birds.js
+++ src/components/animation/birds.js
@@ -13,7 +13,7 @@ function Birds({ el }) {
       THREE: THREE,
       touchControls: true,
       gyroControls: false,
-      minHeight: 1000.0,
+      minHeight: 1280.0,
       minWidth: 200.0,
       scale: 1.0,
       scaleMobile: 1.0,
--- src/components/common/scrolltarget.js
+++ src/components/common/scrolltarget.js
@@ -8,13 +8,11 @@ const ScrollTarget = ({ id }) => {
   const headerHeight = useSelector((state) => state.headerHeght.headerHeght);
   const dispatch = useDispatch();
 
-  console.log(headerHeight);
   useEffect(() => {
     if (elementId === id) {
       const yOffset = -headerHeight;
       const element = myRef.current;
       const y = element.getBoundingClientRect().top + window.scrollY + yOffset;
-      console.log(y);
       window.scrollTo({ top: y, behavior: "smooth" });
       dispatch(setTargetElement(null));
     }
