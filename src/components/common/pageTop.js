aafdf73ab8cd5a9a63a80f1cd89da053010a6da2
suzu <miyuki.takahashi.0526@gmail.com>
2023-09-21 21:38:25 -0400

    fix safari style

--- src/components/common/pageTop.js
+++ src/components/common/pageTop.js
@@ -1,6 +1,6 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faAngleUp } from "@fortawesome/free-solid-svg-icons";
-import { Box, Button } from "@mui/material";
+import { Button } from "@mui/material";
 import { setTargetElement } from "../../redux/slices/scrollSlise";
 import { useDispatch } from "react-redux";
 
